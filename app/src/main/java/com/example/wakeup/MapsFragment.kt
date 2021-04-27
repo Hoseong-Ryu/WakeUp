35b88f92df0a68d553bff48014fe07f69a2b7157
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2021-04-27 16:51:41 +0900

    SearchAndMove

--- app/src/main/java/com/example/wakeup/MapsFragment.kt
+++ app/src/main/java/com/example/wakeup/MapsFragment.kt
@@ -39,6 +39,10 @@ class MapsFragment : Fragment() {
                 val mLat = mResultLocation[0].latitude
                 val mLng = mResultLocation[0].longitude
                 Toast.makeText(context,mLat.toString() + mLng.toString(),Toast.LENGTH_SHORT).show()
+//                OnMapReadyCallback { googleMap ->
+//                    val result = LatLng(mLng)
+//                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(result))
+//                }
             }
             false
         }
@@ -47,15 +51,6 @@ class MapsFragment : Fragment() {
     }
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         val seoul = LatLng(37.5642135, 127.0016985)
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
     }
